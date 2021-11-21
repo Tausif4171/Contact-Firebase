52427012524fdc1792724f00a90fd8e07a775950
Tausif4171 <tausifkhan4171@gmail.com>
2021-11-21 18:25:17 +0530

    State added

--- src/Components/reactContact.js
+++ src/Components/reactContact.js
@@ -1,8 +1,26 @@
 import React from 'react';
+import { useState } from 'react/cjs/react.development';
 import './contact.css';
 
 
 const ContactFirebase = () => {
+
+    const[user,setUser] = useState({
+        first:"",
+        last:"",
+        email:"",
+        phone:"",
+        message:""
+    });
+    var name, value;
+
+    const getUserData = (event) =>{
+        name=event.target.name;
+        value=event.target.value;
+        setUser({...user, [name]: value});
+    }
+
+
     return (
         <>
             <div className="container">
@@ -10,31 +28,31 @@ const ContactFirebase = () => {
                 <form action="#">
                     <div className="form-row">
                         <div className="input-data">
-                            <input type="text" required/>
+                            <input type="text" name="first" value={user.first} onChange={getUserData} />
                             <div className="underline"></div>
                             <label for="">First Name</label>
                         </div>
                         <div className="input-data">
-                            <input type="text" required/>
+                            <input type="text" name="last" value={user.last} onChange={getUserData} />
                             <div className="underline"></div>
                             <label for="">Last Name</label>
                         </div>
                     </div>
                     <div className="form-row">
                         <div className="input-data">
-                            <input type="text" required/>
+                            <input type="text" name="email" value={user.email} onChange={getUserData} />
                             <div className="underline"></div>
                             <label for="">Email Address</label>
                         </div>
                         <div className="input-data">
-                            <input type="text" required/>
+                            <input type="text" name="phone" value={user.phone} onChange={getUserData} />
                             <div className="underline"></div>
-                            <label for="">Website Name</label>
+                            <label for="">Phone Number</label>
                         </div>
                     </div>
                     <div className="form-row">
                         <div className="input-data textarea">
-                            <textarea rows="8" cols="80" required></textarea>
+                            <textarea rows="8" cols="80" name="message" value={user.message} onChange={getUserData}></textarea>
                             <br />
                             <div className="underline"></div>
                             <label for="">Write your message</label>
